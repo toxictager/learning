85e7b4765d31f6ed19ae922f52a729fe3164e97f
toxictager <louilevy1@gmail.com>
2025-12-02 22:00:53 +0200

    did first problem of the week

---
+++ 1/problem set/deep.py
@@ -0,0 +1,8 @@
+#ask for answer
+answer = input(("What is the answer to the great question of life, and everything? ")).lower()
+#is answer correct?
+
+if answer == "42" or answer =="forty two" or answer == "forty-two": #if yes:
+    print("yes")
+else: # if no:
+    print("no")
